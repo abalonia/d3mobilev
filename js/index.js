dbf4576136f1aa2c4f73a3537aaabea818e26e69
abalonia <abalonia@gmail.com>
2018-12-29 22:36:45 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,664 @@
+var json = {
+ "name": "",	
+ "free": false,
+ "photo":"css/logo.gif",
+ "children": [
+   {
+    "name": "Mobile",
+    "free": true,
+    "description": "Interactive authoring tools",
+    "children": [
+     {
+      "name": "Xamarin",
+      "description": "Web-based 'cloud' applications for authoring data visualisations",
+      "free": true,
+      
+     },
+     {
+      "name": "Ionic",
+	  "free": true,
+     },
+	 {
+      "name": "Cordova",
+      "description": "Web-based 'cloud' applications for authoring data visualisations",
+      "free": true,
+      
+     }
+    ]
+   },
+   {
+    "name": "Web",
+    "free": true,
+    "description": "Interactive authoring tools",
+    "children": [
+     {
+      "name": "Django",
+      "description": "Web-based 'cloud' applications for authoring data visualisations",
+      "free": true,
+      
+     },
+	 {
+      "name": "Laravel",
+      "description": "Web-based 'cloud' applications for authoring data visualisations",
+      "free": true,
+     },
+	 {
+      "name": "Angular",
+      "description": "Web-based 'cloud' applications for authoring data visualisations",
+      "free": true,
+     },
+	 
+     {
+      "name": "Ruby on Rails",
+	  "free": true,
+      
+     }
+    ]
+   },
+   {
+    "name": "Database",
+    "description": "Code-based data visualisation creation",
+    "free": true,
+    "children": [
+     {
+      "name": "PostgreSQL",
+      "description": "The language behind most (all?) browser-based data visualisations",
+      "free": true,
+     },
+	 {
+      "name": "Oracle",
+      "description": "The language behind most (all?) browser-based data visualisations",
+      "free": true,
+     },
+	 {
+      "name": "SAP Hana",
+      "description": "The language behind most (all?) browser-based data visualisations",
+      "free": true,
+     },
+	 {
+      "name": "Microsoft SQL",
+      "description": "The language behind most (all?) browser-based data visualisations",
+      "free": true,
+     }
+     
+    ]
+   },
+   {
+    "name": "Data Pipeline",
+    "free": true,
+    "description": "Interactive authoring tools",
+    "children": [
+     {
+      "name": "Automation",
+      "description": "Extract, transform, load automation",
+      "free": true,
+      "children": [
+       {
+        "name": "FME",
+        "description": "Feature Manipulation Engine - Data transformation automation",
+        "url": "https://www.safe.com/",
+        "free": true
+       },
+       {
+        "name": "Python",
+        "description": "Python based data manipulation and ETL automation",
+        "free": true
+       }
+     ]
+     },
+	 {
+      "name": "ETL",
+      "description": "Extract, transform, load automation",
+      "free": true,
+      "children": [
+       {
+        "name": "Bonobo",
+        "description": "Feature Manipulation Engine - Data transformation automation",
+        "url": "https://www.safe.com/",
+        "free": true
+       },
+       {
+        "name": "SQL",
+        "description": "Python based data manipulation and ETL automation",
+        "free": true
+       },
+	    {
+        "name": "Java",
+        "description": "Python based data manipulation and ETL automation",
+        "free": true
+       }
+	   ]
+     },
+     {
+      "name": "Vizualization",
+	  "free": true,
+      "children": [
+	  {
+          "name": "Bokeh",
+          "description": "A powerful tool for producing interactive plots, dashboards and data applications",
+          "url": "https://bokeh.pydata.org/",
+          "free": true
+         },
+	  {
+          "name": "D3",
+          "description": "The jewel in the crown of web-based data visualisation. A library packed full of components for building any data visualisation you can imagine.",
+          "url": "https://d3js.org/",
+          "free": true
+         },
+		 {
+          "name": "Google Charts",
+          "description": "A good selection of charts including bar, line, scatter, geo, pie, donut, org etc.",
+          "url": "https://developers.google.com/chart/",
+          "free": true
+         },
+		 {
+          "name": "HighCharts",
+          "description": "A well maintained commercial library of commonly used chart types",
+          "url": "https://www.highcharts.com/",
+		  "free": true
+         },
+         {
+          "name": "InfoVis",
+          "description": "A lovely selection of charts including bar, pie, sunburst, icicle, network, trees etc.",
+          "url": "https://philogb.github.io/jit/",
+          "free": true
+         },
+		 {
+        "name": "plotly",
+        "description": "Cloud-based interactive tool for creating data visualisations",
+        "url": "https://plot.ly/",
+        "free": true
+       },
+         {
+          "name": "Ractive",
+          "description": "Relatively new, Ractive helps you make your HTML and SVG interactive",
+          "url": "http://www.ractivejs.org/",
+          "free": true
+         },
+         {
+          "name": "Raphaël",
+          "description": "A general purpose drawing library with good browser support",
+          "url": "http://raphaeljs.com/",
+          "free": true
+         },
+		 {
+        "name": "RAW",
+        "description": "Open-source interactive tool for creating and exporting D3-like charts",
+        "url": "http://raw.densitydesign.org/",
+        "free": true
+       },
+         {
+          "name": "Snap.svg",
+          "description": "A modern version of Raphaël that supports modern browsers",
+          "url": "http://snapsvg.io/",
+          "free": true
+         },
+		 {
+        "name": "Tableau",
+        "description": "Powerful tool for data analytics and visualisation",
+        "url": "http://www.tableausoftware.com/products/desktop",
+		"free": true
+		 },
+         {
+          "name": "Variance",
+          "description": "A declarative, mark-up based data visualisation library",
+          "url": "https://variancecharts.com/",
+		  "free": true
+         },
+         {
+          "name": "Vega",
+          "description": "A declarative language for specifying data visualistions",
+          "url": "https://trifacta.github.io/vega/",
+          "free": true
+         }
+       
+	   
+       
+      ]
+     }
+    ]
+   },
+    {
+    "name": "GIS & Geospatial",
+    "free": true,
+    "description": "Interactive authoring tools",
+    "children": [
+      {
+      "name": "Mapping",
+      "description": "Libraries for visualising geographic data",
+      "free": true,
+      "children": [
+		   {
+            "name": "ESRI ArcGIS",
+            "description": "Lovely vector based mapping library with good browser support",
+            "url": "http://kartograph.org/",
+            "free": true
+           },
+		   {
+            "name": "Google Maps",
+            "description": "Lovely vector based mapping library with good browser support",
+            "url": "http://kartograph.org/",
+            "free": true
+           },
+           {
+            "name": "Kartograph",
+            "description": "Lovely vector based mapping library with good browser support",
+            "url": "http://kartograph.org/",
+            "free": true
+           },
+		   {
+            "name": "Leaflet",
+            "description": "Tile-based mapping library",
+            "url": "http://leafletjs.com/",
+            "free": true
+           },
+		   {
+            "name": "Mapbox",
+            "description": "Tile-based mapping library",
+            "url": "http://leafletjs.com/",
+            "free": true
+           },
+		   {
+            "name": "PostGIS",
+            "description": " an open source software program that adds support for geographic objects to the PostgreSQL object-relational database. PostGIS follows the Simple Features for SQL specification from the Open Geospatial Consortium. ",
+            "url": "https://postgis.net/",
+            "free": true
+           },
+		   {
+            "name": "OpenLayers",
+            "description": "an open-source JavaScript library for displaying map data in web browsers as slippy maps. It provides an API for building rich web-based geographic applications similar to Google Maps and Bing Maps.",
+            "url": "https://openlayers.org/",
+            "free": true
+           },
+		   {
+            "name": "OpenStreetMap",
+            "description": "Open and crowdsourced ecosystem of data and tools for mapping",
+            "url": "https://www.osm-tools.org/",
+            "free": true
+           }
+           
+          ]
+      },
+      {
+      "name": "Server",
+	  "description": "Infrastructure for processing geospatial data",
+	  "free": true,
+      "children": [
+       {
+        "name": "ArcGIS Server",
+        "description": "Powerful software suite for connecting, hosting, and sharing geospatial resources such as maps, globes, address locators, geodatabases, and tools globally devices such as tablets, smartphones, laptops, desktop workstations, and any other devices that can connect to web services.",
+        "url": "https://enterprise.arcgis.com/en/",
+		"free": true
+       },
+	   {
+        "name": "GeoServer",
+        "description": "An open-source server written in Java that allows users to share, process and edit geospatial data.",
+        "url": "http://geoserver.org/",
+		"free": true
+       },
+       {
+        "name": "MapServer",
+        "description": "One of the fastest mapping engines in the world, MapServer is an Open Source platform for publishing spatial data and interactive mapping",
+        "url": "https://mapserver.org/",
+        "free": true
+       }
+      ]
+      },
+	  {
+      "name": "Distributed Solutions",
+	  "free": true,
+      "children": [
+       {
+        "name": "ArcGIS Enterprise",
+        "description": "Powerful tool for data analytics and visualisation",
+        "url": "http://www.tableausoftware.com/products/desktop",
+		"free": true,
+		"children": [
+			{
+			"name": "Portal",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "ArcGIS Server",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "ArcGIS Online",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "Image Server",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "GeoEvent Server",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "OpenData",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "Collector",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "Survey 123",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "Data Store",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			},
+			{
+			"name": "GeoAnalytics Server",
+			"description": "Powerful tool for data analytics and visualisation",
+			"url": "http://www.tableausoftware.com/products/desktop",
+			"free": true
+			}
+	     ]
+},
+	   {
+        "name": "GeoServer/Geonode",
+        "description": "Powerful tool for data analytics and visualisation",
+        "url": "http://www.tableausoftware.com/products/desktop",
+		"free": true
+       }
+      
+      ]
+      }
+    ]
+   },
+    {
+    "name": "Data Collection",
+    "free": true,
+    "description": "Interactive authoring tools",
+    "children": [
+     {
+      "name": "Remote Sensing",
+      "description": "Web-based 'cloud' applications for authoring data visualisations",
+      "free": true,
+      "children": [
+       {
+        "name": "Satellite Imagery",
+        "description": "An open-source platform for publishing charts on the web. Cloud-based or self-hosted.",
+        "url": "https://datawrapper.de/",
+        "free": true
+       },
+	   {
+        "name": "Fixed-Wing Drone",
+        "description": "An open-source platform for publishing charts on the web. Cloud-based or self-hosted.",
+        "url": "https://datawrapper.de/",
+        "free": true
+       },
+       {
+        "name": "Quadcopter",
+        "description": "Spreadsheet in the cloud with charting",
+        "free": true
+       }
+       
+      ]
+     },
+     {
+      "name": "Ground Survey",
+	  "free": true,
+      "children": [
+       {
+        "name": "Total Stations",
+        "description": "Powerful tool for data analytics and visualisation",
+        "url": "http://www.tableausoftware.com/products/desktop",
+		"free": true
+       },
+       {
+        "name": "Theodolites",
+        "description": "Free version of Tableau Desktop where charts are public",
+        "url": "http://www.tableausoftware.com/products/public",
+        "free": true
+       },
+	   {
+        "name": "GPS/GLONASS",
+        "description": "Free version of Tableau Desktop where charts are public",
+        "url": "http://www.tableausoftware.com/products/public",
+        "free": true
+       }
+      ]
+     }
+    ]
+   }
+ ]
+};
+
+var width = 500;
+var height = 400;
+var maxLabel = 70;
+var duration = 500;
+var radius = 3;
+
+var i = 0;
+var root;
+
+var tree = d3.layout.tree()
+  .size([height, width]);
+
+/* var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });*/
+
+var lineFunction = d3.svg.line()
+  .x(function(d) {
+    return d.x;
+  })
+  .y(function(d) {
+    return d.y;
+  })
+  .interpolate("linear");
+
+var svg = d3.select("body").append("svg")
+   .attr("viewBox", "0 0 " + width + " " + height )
+  .attr("preserveAspectRatio", "xMidYMid meet")
+  .append("g")
+  .attr("transform", "translate(" + maxLabel + ",25)");
+
+root = json;
+root.x0 = 0;
+root.y0 = width / 2;
+
+root.children.forEach(collapse);
+
+function update(source) {
+  // Compute the new tree layout.
+  var nodes = tree.nodes(root).reverse();
+  var links = tree.links(nodes);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d) {
+    d.y = d.depth * maxLabel;
+  });
+
+  // Update the nodes…
+  var node = svg.selectAll("g.node")
+    .data(nodes, function(d) {
+      return d.id || (d.id = ++i);
+    });
+
+
+  // Update the links…
+  var link = svg.selectAll(".link")
+    .data(links)
+	
+
+  // Enter any new links at the parent's previous position.
+  link.enter().append("line")
+    .attr("class", "link")
+    .attr("x1", function(d) {
+      return d.source.x;
+    })
+    .attr("y1", function(d) {
+      return d.source.y;
+    })
+    .attr("x2", function(d) {
+      return d.target.x;
+    })
+    .attr("y2", function(d) {
+      return d.target.y;
+    });
+
+  // Transition links to their new position.
+  link.transition()
+    .duration(duration)
+    .attr("d", lineFunction)
+    .attr("x1", function(d) {
+      return d.source.x;
+    })
+    .attr("y1", function(d) {
+      return d.source.y;
+    })
+    .attr("x2", function(d) {
+      return d.target.x;
+    })
+    .attr("y2", function(d) {
+      return d.target.y;
+    });;
+
+  // Transition exiting nodes to the parent's new position.
+  link.exit()
+    .remove();
+  // Enter any new nodes at the parent's previous position.
+  var nodeEnter = node.enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) {
+      return "translate(" + source.x0 + "," + source.y0 + ")";
+    })
+    .on("click", click);
+
+  nodeEnter.append("svg:circle")
+    .attr("r", 0)
+    .style("fill", function(d) { return d._children ? node.append("circle").classed("pulse",true)
+    	.attr('r',5)
+    	.style({'stroke': 'white', 'stroke-width': 1, 'fill': '#1564b0'}) : "white"; });
+
+	 nodeEnter.append('a')
+      .attr('xlink:href', function(d) {
+        return d.url;
+      })	
+		
+  nodeEnter.append("text")
+    .attr("x", function(d) {
+      var spacing = computeRadius(d) + 5;
+      return d.children || d._children ? -spacing : spacing;
+    })
+    .attr("dy", "3")
+    .attr("text-anchor", function(d) {
+      return d.children || d._children ? "end" : "start";
+    })
+    .text(function(d) {
+      return d.name;
+    })
+    .style("fill-opacity", 0)
+	
+            .attr("transform", "rotate(90)" )
+	;
+
+  // Transition nodes to their new position.
+  var nodeUpdate = node.transition()
+    .duration(duration)
+    .attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
+
+  nodeUpdate.select("circle")
+    .attr("r", function(d) {
+      return computeRadius(d);
+    })
+    .style("fill", function(d) {
+      return d._children ? "#1564b0" : "white";
+    });
+
+  nodeUpdate.select("text").style("fill-opacity", 1);
+
+  // Transition exiting nodes to the parent's new position.
+  var nodeExit = node.exit().transition()
+    .duration(duration)
+    .attr("transform", function(d) {
+      return "translate(" + source.x + "," + source.y + ")";
+    })
+    .remove();
+
+  nodeExit.select("circle").attr("r", 0);
+  nodeExit.select("text").style("fill-opacity", 0);
+
+  // Stash the old positions for transition.
+  nodes.forEach(function(d) {
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+}
+
+function computeRadius(d) {
+  if (d.children || d._children) return radius + (radius * nbEndNodes(d) / 10);
+  else return radius;
+}
+
+function nbEndNodes(n) {
+  nb = 0;
+  if (n.children) {
+    n.children.forEach(function(c) {
+      nb += nbEndNodes(c);
+    });
+  } else if (n._children) {
+    n._children.forEach(function(c) {
+      nb += nbEndNodes(c);
+    });
+  } else nb++;
+
+  return nb;
+}
+
+function click(d) {
+  if (d.children) {
+	d._children = d.children;
+    d.children = null;
+	if (d.name === ""){
+		//alert("outside");
+	}
+  } else  {
+   // alert("inside");
+	d.children = d._children;
+    d._children = null;
+  }
+
+  update(d);
+}
+
+
+function collapse(d) {
+  if (d.children) {
+    d._children = d.children;
+    d._children.forEach(collapse);
+    d.children = null;
+	collapse(root);
+   }
+}
+
+update(root);
+
+
+
